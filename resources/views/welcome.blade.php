fddd9dca362f10b0dc5d4a64c7a02531d23f88a2
Ken Lin <ken.lin@cloud-interactive.com>
2015-11-05 20:32:03 +0800

    built a simple form to send sms.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,45 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+    <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet"
+          type="text/css">
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+    <style>
+        input, select {
+            margin-top: 0.8em;
+        }
+        select {
+            font-size: 1.7em;
+        }
+    </style>
+</head>
+<body>
+<div class="container col-md-4 col-md-offset-4">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <form class="form-sms" action="/sms" method="POST">
+        <h2 class="form-sms-heading">Send SMS</h2>
+        <label for="inputPhone" class="sr-only">Phone Number</label>
+        <input type="text" id="inputPhone" class="form-control"
+               placeholder="Cell Phone #, ex: 0955997887" required autofocus>
+        <label for="inputMessage" class="sr-only">Message</label>
+        <input type="text" id="inputMessage" class="form-control"
+               placeholder="Enter your message" required>
+        <label for="platform">Select Platform:</label>
+        <select name="platform" id="platform">
+            <option value="p1">p1</option>
+            <option value="p2">p2</option>
+            <option value="p3">p3</option>
+        </select>
+        <br/>
+        <br/>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Send</button>
+    </form>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+</div> <!-- /container -->
+</body>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </html>
